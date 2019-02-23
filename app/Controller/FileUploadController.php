241b8fc3d509398834aa21e59c1317e60999856f
kurtisassad <kurtisassad@gmail.com>
2019-02-23 09:44:12 -0500

    finshed q3

--- app/Controller/FileUploadController.php
+++ app/Controller/FileUploadController.php
@@ -3,8 +3,8 @@
 class FileUploadController extends AppController {
 	public function index() {
 		$this->set('title', __('File Upload Answer'));
-
 		$file_uploads = $this->FileUpload->find('all');
 		$this->set(compact('file_uploads'));
+		}
 	}
 }

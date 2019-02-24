3ff3d7d19fdf5f2f3cd076652d2bd696919f6d61
kurtisassad <kurtisassad@gmail.com>
2019-02-24 09:54:35 -0500

    finished q5

--- app/Controller/OrderController.php
+++ app/Controller/OrderController.php
@@ -1,15 +1,15 @@
 <?php
 	class OrderController extends AppController{
-		
+
 		public function index(){
 			ini_set('memory_limit','256M');
-			
+
 			$orders = $this->Order->find('all',array('conditions'=>array('Order.valid'=>1),'recursive'=>2));
 
 			// debug($orders);exit;
 
 			$this->set('orders',$orders);
-			
+
 			$this->set('title',__('List Orders'));
 		}
 
@@ -19,7 +19,7 @@
 
 				$this->loadModel('OrderDetail');
 				$order_details = $this->OrderDetail->find('all',array('conditions'=>array('OrderDetail.valid'=>1,'OrderDetail.order_id'=>$id)));
-				
+
 				// debug($order_details);exit;
 
 				$this->set('order_details',$order_details);
@@ -29,5 +29,5 @@
 			}
 
 		}
-		
+
 	}
